770390130e7fe10a632eac4bba01fe56fde78a37
Sam David <yaboiwizzysam@gmail.com>
2025-10-07 15:35:58 +0700

    fixed missing landingpage controller on web.php
    fixed seeder for edit pengaduan
    
    fix profile
    
    fix nomor telepon and program studi now showing on profile page
    
    fix user in kelola user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,6 +12,12 @@ use App\Models\Prodi;
 use App\Models\JenisPekerjaan;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
+<<<<<<< HEAD
+=======
+use App\Models\Prodi;
+use App\Models\JenisPekerjaan;
+>>>>>>> 58272ab (fixed missing landingpage controller on web.php)
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 
@@ -101,6 +107,7 @@ class AdminController extends Controller
         $prodis = Prodi::orderBy('nama_prodi', 'asc')->get();
         $jenisPekerjaan = JenisPekerjaan::all();
         $admins = Admin::paginate(5, ['*'], 'admin_page');
+<<<<<<< HEAD
 
         $query = User::with('prodifk', 'pekerjaanfk');
 
@@ -123,6 +130,10 @@ class AdminController extends Controller
 
         $users = $query->latest()->paginate(5)->withQueryString();
 
+=======
+        $prodis = Prodi::orderBy('nama_prodi')->get();
+        $jenisPekerjaan = JenisPekerjaan::orderBy('nama_pekerjaan')->get();
+>>>>>>> c167a67 (fix user in kelola user)
         return view('pages.admin.kelola-user', [
             'totalUsers' => $totalUsers,
             'totalMahasiswa' => $totalMahasiswa,
@@ -131,7 +142,11 @@ class AdminController extends Controller
             'users' => $users,
             'admins' => $admins,
             'prodis' => $prodis,
+<<<<<<< HEAD
             'jenisPekerjaan' => $jenisPekerjaan
+=======
+            'jenisPekerjaan' =>$jenisPekerjaan,
+>>>>>>> c167a67 (fix user in kelola user)
         ]);
     }
     public function storeUser(Request $request)
@@ -160,6 +175,7 @@ class AdminController extends Controller
     }
 
 
+<<<<<<< HEAD
 
     public function profilIndex()
     {
