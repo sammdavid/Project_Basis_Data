90a75aa71cd703ad5819c6aeba802777385fbeb9
Sam David <yaboiwizzysam@gmail.com>
2025-10-07 23:24:29 +0700

    fix tambah user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -101,7 +101,6 @@ class AdminController extends Controller
         $prodis = Prodi::orderBy('nama_prodi', 'asc')->get();
         $jenisPekerjaan = JenisPekerjaan::all();
         $admins = Admin::paginate(5, ['*'], 'admin_page');
-<<<<<<< HEAD
 
         $query = User::with('prodifk', 'pekerjaanfk');
 
@@ -124,10 +123,6 @@ class AdminController extends Controller
 
         $users = $query->latest()->paginate(5)->withQueryString();
 
-=======
-        $prodis = Prodi::orderBy('nama_prodi')->get();
-        $jenisPekerjaan = JenisPekerjaan::orderBy('nama_pekerjaan')->get();
->>>>>>> c167a67 (fix user in kelola user)
         return view('pages.admin.kelola-user', [
             'totalUsers' => $totalUsers,
             'totalMahasiswa' => $totalMahasiswa,
@@ -136,11 +131,7 @@ class AdminController extends Controller
             'users' => $users,
             'admins' => $admins,
             'prodis' => $prodis,
-<<<<<<< HEAD
             'jenisPekerjaan' => $jenisPekerjaan
-=======
-            'jenisPekerjaan' =>$jenisPekerjaan,
->>>>>>> c167a67 (fix user in kelola user)
         ]);
     }
     public function storeUser(Request $request)
